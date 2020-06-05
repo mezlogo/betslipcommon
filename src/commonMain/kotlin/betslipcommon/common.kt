ae8b6e7f6c4236562063e3bee85b8f24582e4964
mezentsev.d <dmezentsev@marathonbet.ru>
2020-06-05 17:39:22 +0300

    init

--- src/commonMain/kotlin/betslipcommon/common.kt
+++ src/commonMain/kotlin/betslipcommon/common.kt
@@ -4,10 +4,55 @@ package betslipcommon
 
 import kotlin.js.JsExport
 
-data class ResponseDataClass(val msg: String, val code: Int)
-
-interface Callback {
-    fun onCall(msg: String): String
+interface BetslipApi {
+    fun addRmChoice(clickedChoice: Choice)
+    fun rmChoice(clickedChoice: Choice)
+    fun clear()
+    fun placeBet()
+    fun setStake()
 }
 
-fun commonCode(callback: Callback) = ResponseDataClass(callback.onCall("fromCommon"), 2)
+data class Choice(
+        val eventId: Long,
+        val selectionUid: String,
+        val coeffId: Long,
+)
+
+interface BetslipStorageSpi {
+    fun getSelectedChoices() : List<Choice>
+    fun addChoice(clickedChoice: Choice)
+    fun removeChoice(clickedChoice: Choice)
+    fun clear()
+}
+
+class BetslipApiImpl(val storageSpi: BetslipStorageSpi) : BetslipApi {
+    private fun isEventIdAndUidEqual(l: Choice, r: Choice) = l.eventId == r.eventId && l.selectionUid == r.selectionUid
+
+    override fun addRmChoice(clickedChoice: Choice) {
+        val selectedChoices = storageSpi.getSelectedChoices()
+        val foundChoice = selectedChoices.firstOrNull { isEventIdAndUidEqual(it, clickedChoice) }
+        if (null == foundChoice)
+            storageSpi.addChoice(clickedChoice)
+        else
+            storageSpi.removeChoice(clickedChoice)
+    }
+
+    override fun rmChoice(clickedChoice: Choice) {
+        val selectedChoices = storageSpi.getSelectedChoices()
+        val foundChoice = selectedChoices.firstOrNull { isEventIdAndUidEqual(it, clickedChoice) }
+        if (null != foundChoice)
+            storageSpi.removeChoice(clickedChoice)
+    }
+
+    override fun clear() {
+        storageSpi.clear()
+    }
+
+    override fun placeBet() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setStake() {
+        TODO("Not yet implemented")
+    }
+}
