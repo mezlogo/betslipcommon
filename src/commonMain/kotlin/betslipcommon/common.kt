ce18912f69f7835625a920b544e33bfa56d16931
mezentsev.d <dmezentsev@marathonbet.ru>
2020-06-15 20:27:09 +0300

    init

--- src/commonMain/kotlin/betslipcommon/common.kt
+++ src/commonMain/kotlin/betslipcommon/common.kt
@@ -4,6 +4,11 @@ package betslipcommon
 
 import kotlin.js.JsExport
 
+//Supports up to 5 choices
+enum class BetType { SINGLE, ANTIEXPRESS, DOUBLE, DOUBLES, TREBLE, TREBELS, ACCUMULATOR, FOLD4, PATENT, TRIXIE, YANKEE, LUCKY15, SUPER_YANKEE, LUCKY31 }
+
+enum class BetslipTab { SINGLES, ACCUMULATORS, ANTIEXPRESSES, MULTIPLES }
+
 data class SingleBetCalcResult(val totalReturns: Stake, val totalStake: Stake)
 data class Fraction(val numerator: Long, val denumerator: Long)
 data class SelectionRef(val eventId: Long, val selectionUid: String)
@@ -11,16 +16,30 @@ data class Choice(val selectionRef: SelectionRef, var coeffId: Long, var coeff: 
 data class Stake(val value: Float) {
     fun add(stake: Stake) = Stake(value + stake.value)
 }
+
 data class Bet(var stake: Stake, var minStake: Stake, var maxStake: Stake)
 data class SingleBet(val choice: Choice, val bet: Bet, var potentialReturns: Stake = Stake(0f))
-enum class PlaceBetStatus { OK, LIVE_DELAY, ERROR}
+enum class PlaceBetStatus { OK, LIVE_DELAY, ERROR }
 data class PlaceBetResponse(val status: PlaceBetStatus)
 
 interface BetslipModel {
     fun addChoice(newChoice: Choice): Boolean
     fun removeChoice(selectionRef: SelectionRef): Boolean
-    fun setStake(selectionRef: SelectionRef, stakeAsString: String): Boolean
     fun placeBet()
+    fun selectSinglesTab(): SinglesBetslipModel
+    fun selectAccumulatorsTab(): ComplexBetslipModel
+    fun selectAntiexpressTab(): ComplexBetslipModel
+    fun selectMultipleTab(): ComplexBetslipModel
+}
+
+interface SinglesBetslipModel : BetslipModel {
+    fun setSingleStake(selectionRef: SelectionRef, stake: Stake): Boolean
+    fun getSingleBets(): List<SingleBet>
+}
+
+interface ComplexBetslipModel : BetslipModel {
+    fun setStake(betType: BetType, stake: Stake): Boolean
+    fun getBetsForSelectedBetslipTab(): List<Bet>
 }
 
 interface BetslipStorageSpi {
@@ -30,16 +49,65 @@ interface BetslipStorageSpi {
     fun betIsPlacedSuccessfully()
     fun setSingleBets(singleBets: List<SingleBet>, singleBetCalcResult: SingleBetCalcResult)
     fun noChoicesLeft()
+    fun getSelectedBetslipTab(): BetslipTab
+    fun setSelectedBetslipTab(selectedBetslipTab: BetslipTab)
 }
+
+data class CheckBetsAoSpiResponse(val minStake: Stake, val maxStake: Stake)
 
 interface BetslipAoSpi {
-    fun addChoice(selectedChoices: List<Choice>, clickedChoice: Choice): Bet
-    fun placeSingleBets(bets: List<SingleBet>) : PlaceBetResponse
+    fun getAvailableBetTypes(choices: List<Choice>): List<BetType>
+    fun checkBets(bets: List<Bet>): List<CheckBetsAoSpiResponse>
+    fun placeSingleBets(bets: List<SingleBet>): PlaceBetResponse
 }
 
-class BetslipModelCommon(val storageSpi: BetslipStorageSpi, val aoSpi: BetslipAoSpi) : BetslipModel {
-    private fun validateStake(stakeAsString: String) = Stake(stakeAsString.toFloat())
+class BetslipModelCommon(val storageSpi: BetslipStorageSpi, val aoSpi: BetslipAoSpi) : SinglesBetslipModel, ComplexBetslipModel {
+
+    private fun mapBetTypeToBetslipTabs(betType: BetType) = when (betType) {
+        BetType.SINGLE -> BetslipTab.SINGLES
+        BetType.ANTIEXPRESS -> BetslipTab.ANTIEXPRESSES
+
+        BetType.DOUBLE, BetType.DOUBLES, BetType.TREBLE, BetType.TREBELS,
+        BetType.ACCUMULATOR, BetType.FOLD4 -> BetslipTab.ACCUMULATORS
+
+        else -> BetslipTab.MULTIPLES
+    }
+
+
     private fun calcReturns(stake: Stake, coeff: Fraction) = Stake(stake.value * coeff.numerator / coeff.denumerator)
+
+    override fun addChoice(newChoice: Choice): Boolean {
+        val alreadySelectedChoices = storageSpi.getSelectedChoices()
+
+        val isChoiceAlreadySelected = null != alreadySelectedChoices.firstOrNull { newChoice.selectionRef == it.selectionRef }
+
+        if (isChoiceAlreadySelected) {
+            return false
+        }
+
+        val choices = ArrayList(alreadySelectedChoices)
+        choices.add(newChoice)
+
+        val availableBetTypes = aoSpi.getAvailableBetTypes(choices)
+        var selectedBetslipTab = storageSpi.getSelectedBetslipTab()
+        val betTypesByBetlipTabs = availableBetTypes.groupBy { mapBetTypeToBetslipTabs(it) }
+
+        if (!betTypesByBetlipTabs.containsKey(selectedBetslipTab)) {
+            selectedBetslipTab = BetslipTab.SINGLES
+            storageSpi.setSelectedBetslipTab(selectedBetslipTab)
+        }
+
+        if (BetslipTab.SINGLES == selectedBetslipTab) {
+            val singleBets = getSingleBets().map { it.bet }
+            val checkBets = aoSpi.checkBets(singleBets)
+        } else {
+            val selectedBetTypes = betTypesByBetlipTabs[selectedBetslipTab]
+        }
+
+        storageSpi.addChoice(newChoice, SingleBet(newChoice, bet))
+
+        return true
+    }
 
     private fun calcTotalsForSingleBets(singleBets: List<SingleBet>): SingleBetCalcResult {
         val totalReturns = singleBets.map { calcReturns(it.bet.stake, it.choice.coeff) }.reduce { l, r -> l.add(r) }
@@ -47,21 +115,35 @@ class BetslipModelCommon(val storageSpi: BetslipStorageSpi, val aoSpi: BetslipAo
         return SingleBetCalcResult(totalReturns, totalStake)
     }
 
-    override fun addChoice(newChoice: Choice): Boolean {
-        val selectedChoices = storageSpi.getSelectedChoices()
+    override fun setSingleStake(selectionRef: SelectionRef, stake: Stake): Boolean {
+        val singleBets = storageSpi.getSingleBets()
 
-        val selectedChoice = selectedChoices.firstOrNull { newChoice.selectionRef == it.selectionRef }
+        val singleBet = singleBets.firstOrNull { it.choice.selectionRef == selectionRef }
 
-        if (null != selectedChoice) {
+        if (null == singleBet) {
             return false
         }
 
-        val bet = aoSpi.addChoice(selectedChoices, newChoice)
+        singleBet.bet.stake = stake
+        singleBet.potentialReturns = calcReturns(stake, singleBet.choice.coeff)
 
-        storageSpi.addChoice(newChoice, SingleBet(newChoice, bet))
+        storageSpi.setSingleBets(singleBets, calcTotalsForSingleBets(singleBets))
 
         return true
     }
+
+    override fun getSingleBets(): List<SingleBet> {
+        TODO("Not yet implemented")
+    }
+
+    override fun setStake(betType: BetType, stake: Stake): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getBetsForSelectedBetslipTab(): List<Bet> {
+        TODO("Not yet implemented")
+    }
+
 
     override fun removeChoice(selectionRef: SelectionRef): Boolean {
         val selectedBets = storageSpi.getSingleBets()
@@ -83,24 +165,6 @@ class BetslipModelCommon(val storageSpi: BetslipStorageSpi, val aoSpi: BetslipAo
         return true
     }
 
-    override fun setStake(selectionRef: SelectionRef, stakeAsString: String): Boolean {
-        val stake = validateStake(stakeAsString)
-        val singleBets = storageSpi.getSingleBets()
-
-        val singleBet = singleBets.firstOrNull { it.choice.selectionRef == selectionRef }
-
-        if (null == singleBet) {
-            return false
-        }
-
-        singleBet.bet.stake = stake
-        singleBet.potentialReturns = calcReturns(stake, singleBet.choice.coeff)
-
-        storageSpi.setSingleBets(singleBets, calcTotalsForSingleBets(singleBets))
-
-        return true
-    }
-
     override fun placeBet() {
         val singleBets = storageSpi.getSingleBets()
 
@@ -116,4 +180,9 @@ class BetslipModelCommon(val storageSpi: BetslipStorageSpi, val aoSpi: BetslipAo
             PlaceBetStatus.OK -> storageSpi.betIsPlacedSuccessfully()
         }
     }
+
+    override fun selectSinglesTab() = this
+    override fun selectAccumulatorsTab() = this
+    override fun selectAntiexpressTab() = this
+    override fun selectMultipleTab() = this
 }
